35ff32e38fafa5af15c10838a2458f147dc1dfdf
Nolan Kovacik <noltron000@gmail.com>
2018-11-27 17:48:24 -0800

    set up basic handlebars

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+// adding requirements
 const express = require('express')
+const exprHBS = require('express-handlebars');
+const bodyParse = require('body-parser');
+const exprValid = require('express-validator');
+
+// initializing variables
 const app = express()
-var exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'index'}));
-app.set('view engine', 'handlebars');
+// set up handlebars
+app.engine('.hbs', exprHBS({
+	extname: '.hbs',
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
 
-let hello = ('hi')
+// use body parser
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({ extended: false }));
+
+// use validator - adding after parser init!
+app.use(exprValid());
+
+// // use method override - with POST having ?_method=DELETE or ?_method=PUT
+// app.use(express.static('public'));
+// app.use(methodOverride('_method'));
+
+
+
 
 app.get('/', (req, res) => {
-  res.render('index', { hello: hello });
+	res.render('home', { hello: hello });
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`)
 });
