103831fcc310f79ba9cd36c97cafd3c590b646e2
jasonromulus <pumcome@me.com>
2018-11-27 19:02:12 -0800

    new stuff

--- server.js
+++ server.js
@@ -27,10 +27,54 @@ app.use(exprValid());
 // app.use(methodOverride('_method'));
 
 
-
+//INDEX
 app.get('/', (req, res) => {
 	res.render('home');
 });
+
+// NEW
+app.get('/../new', (req, res) => {
+  res.render('..', {});
+  });
+  
+  // SHOW
+  app.get('/../:id', (req, res) => {
+      events.findById(req.params.id).then(events => {
+        Comment.find({ reviewId: req.params.id }).then(events => {
+          res.render('..',)
+        })
+      }).catch((err) => {
+        console.log(err.message)
+      });
+    });
+  
+  // EDIT
+  app.get('/../:id/edit', (req, res) => {
+  events.findById(req.params.id, function(err, review) {
+      res.render('..', {review: review});
+  })
+  });
+  
+  // UPDATE
+  app.put('/../:id', (req, res) => {
+  events.findByIdAndUpdate(req.params.id, req.body)
+      .then(events => {
+      res.redirect(`/../${events._id}`)
+      })
+      .catch(err => {
+      console.log(err.message)
+      })
+  });
+  
+  // DELETE
+  app.delete('/../:id', function (req, res) {
+  console.log("DELETE event")
+  events.findByIdAndRemove(req.params.id).then((event) => {
+      res.redirect('/');
+  }).catch((err) => {
+      console.log(err.message);
+  })
+  });
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}!`)
